5385df0b35f5b46dae134a4a7abd320882f8865a
arcanar7 <arcanar7@gmail.com>
2020-03-03 15:41:06 +0400

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-    // function check(str, bracketsConfig) {
     for (let i = 0; i < bracketsConfig.length; i++) {
         while (true) {
             if (str.includes(bracketsConfig[i].join(""))) {
@@ -11,34 +10,3 @@ module.exports = function check(str, bracketsConfig) {
 
     return str ? false : true;
 };
-
-const config1 = [["(", ")"]];
-const config2 = [
-    ["(", ")"],
-    ["[", "]"]
-];
-const config3 = [
-    ["(", ")"],
-    ["[", "]"],
-    ["{", "}"]
-];
-const config4 = [["|", "|"]];
-const config5 = [
-    ["(", ")"],
-    ["|", "|"]
-];
-const config6 = [
-    ["1", "2"],
-    ["3", "4"],
-    ["5", "6"],
-    ["7", "7"],
-    ["8", "8"]
-];
-const config7 = [
-    ["(", ")"],
-    ["[", "]"],
-    ["{", "}"],
-    ["|", "|"]
-];
-
-// console.log(check("((()))()", config1));
