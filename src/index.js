7639a83de52304a66812ebdc1c30d4f2184a509c
arcanar7 <arcanar7@gmail.com>
2020-03-03 15:31:01 +0400

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // function check(str, bracketsConfig) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        while (true) {
+            if (str.includes(bracketsConfig[i].join(""))) {
+                str = str.replace(bracketsConfig[i].join(""), "");
+                i = 0;
+            } else break;
+        }
+    }
+
+    return str ? false : true;
+};
+
+const config1 = [["(", ")"]];
+const config2 = [
+    ["(", ")"],
+    ["[", "]"]
+];
+const config3 = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"]
+];
+const config4 = [["|", "|"]];
+const config5 = [
+    ["(", ")"],
+    ["|", "|"]
+];
+const config6 = [
+    ["1", "2"],
+    ["3", "4"],
+    ["5", "6"],
+    ["7", "7"],
+    ["8", "8"]
+];
+const config7 = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["|", "|"]
+];
+
+// console.log(check("((()))()", config1));
